7876f8d6cb451fc5178f62547b02e9fcd4c8decd
Amna Amir <aamna48@outlook.com>
2020-08-29 21:30:56 +0500

    Modify ConsoleApp1, Add ConsoleApp2, Add new folder Newget

---
+++ ConsoleApp2/ClassLibrary1/Sort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ClassLibrary1
+
+{
+    public class Sort
+    {
+        public static void BubbleSort(int[] numbers)
+        {
+            Stopwatch sw1 = new Stopwatch();
+            sw1.Start();
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length -1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            sw1.Stop();
+            Console.WriteLine($"Bubble sort took: {sw1.ElapsedMilliseconds} miliseconds");
+        }
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[5000];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(1, 5001);
+            }
+            
+        }
+    }
+}
